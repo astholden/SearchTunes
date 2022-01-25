a954cc10d925c6892b1d372edb9cd1b03d9481e8
astholden <astholden@gmail.com>
2022-01-25 15:55:11 -0500

    working on react

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, {Componenet} from 'react';
+
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
+    };
+
+
+
+
+    render () {
+        return (
+            <div>
+                <input type="button">test</input>
+                <h2>something</h2>
+            </div>
+        );
+    };
+}
+
+export default App;
+
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
-const { mainModule } = require('process');
+// const { mainModule } = require('process');
 
 module.exports = {
 
+    mode: process.env.NODE_ENV,
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
@@ -12,8 +13,20 @@ module.exports = {
         rules: [
           {
             test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
+            use: ["style-loader", "css-loader"],
           },
         ],
+        rules: [
+          {
+            test: /\.jsx?/,
+            exclude: /(node_modules)/,
+            use:  {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+          }
+        ]
     },
 };
