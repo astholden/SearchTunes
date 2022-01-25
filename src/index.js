2c437a7d6864e9c72c81c4cfedc6df7a7d2f4eb7
astholden <astholden@gmail.com>
2022-01-25 14:32:52 -0500

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash'
 function component(){
 const element = document.createElement('div');
 
-element.innerHTML = _.join(['Hello', 'world'], ' ');
+element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
 return element;
 
