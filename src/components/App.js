9930348b2a79a6d84f144e2f298dfdf82c895b58
astholden <astholden@gmail.com>
2022-01-25 20:09:42 -0500

    reverted back

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,57 @@
-import React, {Componenet} from 'react';
+import React, {Component} from 'react';
+
+
+function getInitialState() {
+
+    return {
+        hello: "hello",
+        howAreYou : "Good"
+    }
+
+}
+
+const check = (e) => {
+    e.preventDefault();
+    console.log(value)
+    // fetch('https://itunes.apple.com/search?term=jack+johnson')
+    // .then((data) => data.json())
+    // .then((data) => {
+    //     console.log(data)
+    // })
+}
 
 class App extends Component {
-    
+
+
     constructor(props) {
         super(props);
+        this.state = getInitialState()
+        this.handleChange = this.handleChange.bind(this)
     };
 
 
 
+handleChange = (event) => {
+    console.log(event.target.value)
+    console.log('working')
+    this.setState({test: "test"})
+    console.log(state)
+}
 
-    render () {
+
+
+render () {
+
         return (
             <div>
-                <input type="button">test</input>
-                <h2>something</h2>
+                <form onSubmit={check}>
+                    <input id="search-field" onChange={this.handleChange}></input>
+                    <button id="search">Search</button>
+                </form>
             </div>
-        );
-    };
-}
+        )
+    }
+};
 
 export default App;
 
